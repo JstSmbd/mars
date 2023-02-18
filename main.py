0b76863902b3f0f8f6cf1e5e0e92b4e65c421c6a
JstSmbd <arsengoodvin@yandex.ru>
2023-02-19 01:58:20 +0300

    К задаче "Результат отбора"

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from random import choice
 app = Flask(__name__)
 
 
-@app.route('/choice/<planet>', methods=["POST", "GET"])
-def main(planet):
+@app.route('/results/<nickname>/<int:level>/<float:rating>')
+def main(nickname, level, rating):
     return f'''<!doctype html>
                 <html lang="en">
                   <head>
@@ -16,47 +16,21 @@ def main(planet):
                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
                    crossorigin="anonymous">
                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
-                    <title>Варианты выбора</title>
+                    <title>Результаты</title>
                   </head>
                   <body>
-                    {planets.get(planet.lower(), "<div>Планеты {planet} не найдено</div>").format(planet=planet)}
+                    <h1>Результаты отбора</h1>
+                    <div>Претендента на участие в миссии {nickname}:</div>
+                    <div class="alert alert-success">
+                      Поздравляем! Ваш рейтинг после {level} этапа отбора
+                    </div>
+                    <div>составляет {rating}!</div>
+                    <div class="alert alert-warning">
+                      Желаем удачи!
+                    </div>
                   </body>
                 </html>'''
 
-
-with app.app_context(), app.test_request_context():
-    alerts = ["primary", "secondary", "success", "danger", "warning", "info", "light", "dark"]
-    templates = ['<h1>Мое предложение: {planet}</h1>',
-                 '''<div class="alert alert-{choice_color}">
-                          <div class={choice_font}>{text}</div>
-                          </div>''',
-                 "<img src='{image}' alt='здесь должна была быть картинка, но не нашлась'>"]
-    planets = {
-        "марс": templates[0] + templates[2].format(
-            image=url_for('static', filename="img/mars.png")) +
-                "".join([templates[1].format(choice_color=choice(alerts),
-                                             choice_font=choice(
-                                                 ["bold", "italic",
-                                                  "oblique", "line"]),
-                                             text=text, planet="{planet}")
-                         for text in ["Эта планета близка к Земле;",
-                                      "На ней много необходимых ресурсов;",
-                                      "На ней есть вода и атмосфера;",
-                                      "На ней есть небольшое магнитное поле;",
-                                      "Наконец, она просто красива!"]]),
-        "луна": templates[0] + "".join([templates[1].format(choice_color=choice(alerts),
-                                                            choice_font=choice(
-                                                                ["bold", "italic",
-                                                                 "oblique", "line"]),
-                                                            text=text, planet="{planet}")
-                                        for text in ["Луна является спутником Земли;",
-                                                     "Она в 50 раз меньше Земли по объему;",
-                                                     "У Луны нет магнитного поля;",
-                                                     "12 человек уже побывало на Луне;",
-                                                     "Скоро будет и наша очередь!"]]),
-        "юпитер": templates[0] +
-                  templates[2].format(image=url_for('static', filename="img/jupiter.png"))
-                  + "<div class='bold'>Самая большая планета в Солнечной системе</div>"}
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
