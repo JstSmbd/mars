18e6b493db555182e605b15d8a147662eec99231
JstSmbd <arsengoodvin@yandex.ru>
2023-02-18 22:35:12 +0300

    К задаче "Отбор астронавтов"

--- main.py
+++ main.py
@@ -1,106 +1,86 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, request
+
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def mission():
-    return """<!doctype html>
-                    <html lang="en">
-                      <head>
-                        <meta charset="utf-8">
-                        <title>Колонизация Марса</title>
-                      </head>
-                      <body>
-                        <h1>Миссия Колонизация Марса</h1>
-                      </body>
-                    </html>"""
-
-
-@app.route("/index")
-def deviz():
-    return """<!doctype html>
-                    <html lang="en">
-                      <head>
-                        <meta charset="utf-8">
-                        <title>Девиз</title>
-                      </head>
-                      <body>
-                        <h1>И на Марсе будут яблони цвести!</h1>
-                      </body>
-                    </html>"""
-
-
-@app.route("/promotion")
-def ad():
-    return """<!doctype html>
-                    <html lang="en">
-                      <head>
-                        <meta charset="utf-8">
-                        <title>Реклама</title>
-                      </head>
-                      <body>
-                        <h1>Человечество вырастает из детства.</h1>
-                        <h1>Человечеству мала одна планета.</h1>
-                        <h1>Мы сделаем обитаемыми безжизненные пока планеты.</h1>
-                        <h1>И начнем с Марса!</h1>
-                        <h1>Присоединяйся!</h1>
-                      </body>
-                    </html>"""
-
-
-@app.route("/image_mars")
-def image_mars():
-    return f"""<!doctype html>
-                    <html lang="en">
-                      <head>
-                        <meta charset="utf-8">
-                        <title>Привет, Марс!</title>
-                      </head>
-                      <body>
-                        <h1>Жди нас, Марс!</h1>
-                        <img src="{url_for('static', filename='img/mars.png')}" 
-                        alt="здесь должна была быть картинка, но не нашлась">
-                        <div>Вот она какая, красная планета.</div>
-                      </body>
-                    </html>"""
-
-
-@app.route('/promotion_image')
-def bootstrap():
-    return f'''<!doctype html>
-                <html lang="en">
-                  <head>
-                    <meta charset="utf-8">
-                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                    <link rel="stylesheet" 
-                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
-                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
-                    crossorigin="anonymous">
-                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
-                    <title>Колонизация</title>
-                  </head>
-                  <body>
-                    <h1>Жди нас, Марс!</h1>
-                    <img src="{url_for('static', filename='img/mars.png')}" 
-                    alt="здесь должна была быть картинка, но не нашлась">
-                    <div class="alert-dark" role="alert">
-                      Человечество вырастает из детства.
-                    </div>
-                    <div class="alert-success" role="alert">
-                      Человечеству мала одна планета.
-                    </div>
-                    <div class="alert-secondary" role="alert">
-                      Мы сделаем обитаемыми безжизненные пока планеты.
-                    </div>
-                    <div class="alert-warning" role="alert">
-                      И начнем с Марса!
-                    </div>
-                    <div class="alert-danger" role="alert">
-                      Присоединяйся!
-                    </div>
-                  </body>
-                </html>'''
+@app.route('/astronaut_selection', methods=["POST", "GET"])
+def main():
+    if request.method == "GET":
+        return f"""<!doctype html>
+                        <html lang="en">
+                          <head>
+                            <meta charset="utf-8">
+                            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                            <link rel="stylesheet"
+                            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                            integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                            crossorigin="anonymous">
+                            <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                            <title>Отбор астронавтов</title>
+                          </head>
+                          <body>
+                            <h1 class="center"> Анкета претендента</h1>
+                            <div class="center">на участие в миссии</div>
+                            <div>
+                                <form class="login_form" method="post" enctype="multipart/form-data">
+                                    <input type="text" class="form-control" id="surname" placeholder="Введите фамилию" name="surname">
+                                    <input type="text" class="form-control" id="name" placeholder="Введите имя" name="name">
+                                    <div><br></div>
+                                    <input type="email" class="form-control" id="email" placeholder="Введите адрес почты" name="email">
+                                    <div class="form-group">
+                                        <label for="classSelect">Какое у вас образование?</label>
+                                        <select class="form-control" id="classSelect" name="class">
+                                          <option>Начальное</option>
+                                          <option>Основное общее</option>
+                                          <option>Среднее общее</option>
+                                          <option>Среднее профессиональное</option>
+                                          <option>Высшее</option>
+                                        </select>
+                                    </div>
+                                    <div>Какие у вас есть профессии?</div>
+                                    {chr(10).join([f'''<div class="form-group form-check">
+                                        <input type="checkbox" class="profession-label" id="profession" name="accept" value="{value}">
+                                        <label class="profession-label" for="acceptRules">{value}</label>
+                                    </div>''' for value in ["Инженер-исследователь", "Инженер-строитель", 
+                                                            "Пилот", "Метеоролог", "Инженер по жизнеобеспечению",
+                                                            "Инженер по радиационной защите", "Врач",
+                                                            "Экзобиолог"]])}
+                                    <div class="form-group">
+                                        <label for="form-check">Укажите пол</label>
+                                        <div class="form-check">
+                                          <input class="form-check-input" type="radio" name="le" id="male" value="male" checked>
+                                          <label class="form-check-label" for="male">
+                                            Мужской
+                                          </label>
+                                        </div>
+                                        <div class="form-check">
+                                          <input class="form-check-input" type="radio" name="le" id="female" value="female">
+                                          <label class="form-check-label" for="female">
+                                            Женский
+                                          </label>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="about">Почему вы хотите принять участие в миссии?</label>
+                                        <textarea class="form-control" id="about" rows="3" name="about"></textarea>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="photo">Приложите фотографию</label>
+                                        <input type="file" class="form-control-file" id="photo" name="file">
+                                    </div>
+                                    <div class="form-group form-check">
+                                        <input type="checkbox" class="form-check-input" id="acceptRules" name="accept">
+                                        <label class="form-check-label" for="acceptRules">Готовы остаться на Марсе?</label>
+                                    </div>
+                                    <button type="submit" class="btn btn-primary">Отправить</button>
+                                </form>
+                            </div>
+                          </body>
+                        </html>"""
+    elif request.method == 'POST':
+        print(request.form, request.files)
+        return "Форма отправлена"
 
 
 if __name__ == '__main__':
