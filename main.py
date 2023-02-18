515b2063e9467dc47b41332b91fec7f6c556e03a
JstSmbd <arsengoodvin@yandex.ru>
2023-02-19 01:36:36 +0300

    К задаче "Варианты выбора"

--- main.py
+++ main.py
@@ -1,87 +1,62 @@
 from flask import Flask, url_for, request
-
+from random import choice
 
 app = Flask(__name__)
 
 
-@app.route('/astronaut_selection', methods=["POST", "GET"])
-def main():
-    if request.method == "GET":
-        return f"""<!doctype html>
-                        <html lang="en">
-                          <head>
-                            <meta charset="utf-8">
-                            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                            <link rel="stylesheet"
-                            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
-                            integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
-                            crossorigin="anonymous">
-                            <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
-                            <title>Отбор астронавтов</title>
-                          </head>
-                          <body>
-                            <h1 class="center"> Анкета претендента</h1>
-                            <div class="center">на участие в миссии</div>
-                            <div>
-                                <form class="login_form" method="post" enctype="multipart/form-data">
-                                    <input type="text" class="form-control" id="surname" placeholder="Введите фамилию" name="surname">
-                                    <input type="text" class="form-control" id="name" placeholder="Введите имя" name="name">
-                                    <div><br></div>
-                                    <input type="email" class="form-control" id="email" placeholder="Введите адрес почты" name="email">
-                                    <div class="form-group">
-                                        <label for="classSelect">Какое у вас образование?</label>
-                                        <select class="form-control" id="classSelect" name="class">
-                                          <option>Начальное</option>
-                                          <option>Основное общее</option>
-                                          <option>Среднее общее</option>
-                                          <option>Среднее профессиональное</option>
-                                          <option>Высшее</option>
-                                        </select>
-                                    </div>
-                                    <div>Какие у вас есть профессии?</div>
-                                    {chr(10).join([f'''<div class="form-group form-check">
-                                        <input type="checkbox" class="profession-label" id="profession" name="accept" value="{value}">
-                                        <label class="profession-label" for="acceptRules">{value}</label>
-                                    </div>''' for value in ["Инженер-исследователь", "Инженер-строитель", 
-                                                            "Пилот", "Метеоролог", "Инженер по жизнеобеспечению",
-                                                            "Инженер по радиационной защите", "Врач",
-                                                            "Экзобиолог"]])}
-                                    <div class="form-group">
-                                        <label for="form-check">Укажите пол</label>
-                                        <div class="form-check">
-                                          <input class="form-check-input" type="radio" name="le" id="male" value="male" checked>
-                                          <label class="form-check-label" for="male">
-                                            Мужской
-                                          </label>
-                                        </div>
-                                        <div class="form-check">
-                                          <input class="form-check-input" type="radio" name="le" id="female" value="female">
-                                          <label class="form-check-label" for="female">
-                                            Женский
-                                          </label>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="about">Почему вы хотите принять участие в миссии?</label>
-                                        <textarea class="form-control" id="about" rows="3" name="about"></textarea>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="photo">Приложите фотографию</label>
-                                        <input type="file" class="form-control-file" id="photo" name="file">
-                                    </div>
-                                    <div class="form-group form-check">
-                                        <input type="checkbox" class="form-check-input" id="acceptRules" name="accept">
-                                        <label class="form-check-label" for="acceptRules">Готовы остаться на Марсе?</label>
-                                    </div>
-                                    <button type="submit" class="btn btn-primary">Отправить</button>
-                                </form>
-                            </div>
-                          </body>
-                        </html>"""
-    elif request.method == 'POST':
-        print(request.form, request.files)
-        return "Форма отправлена"
+@app.route('/choice/<planet>', methods=["POST", "GET"])
+def main(planet):
+    return f'''<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                   <link rel="stylesheet"
+                   href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                   integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                   crossorigin="anonymous">
+                   <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                    <title>Варианты выбора</title>
+                  </head>
+                  <body>
+                    {planets.get(planet.lower(), "<div>Планеты {planet} не найдено</div>").format(planet=planet)}
+                  </body>
+                </html>'''
 
+
+with app.app_context(), app.test_request_context():
+    alerts = ["primary", "secondary", "success", "danger", "warning", "info", "light", "dark"]
+    templates = ['<h1>Мое предложение: {planet}</h1>',
+                 '''<div class="alert alert-{choice_color}">
+                          <div class={choice_font}>{text}</div>
+                          </div>''',
+                 "<img src='{image}' alt='здесь должна была быть картинка, но не нашлась'>"]
+    planets = {
+        "марс": templates[0] + templates[2].format(
+            image=url_for('static', filename="img/mars.png")) +
+                "".join([templates[1].format(choice_color=choice(alerts),
+                                             choice_font=choice(
+                                                 ["bold", "italic",
+                                                  "oblique", "line"]),
+                                             text=text, planet="{planet}")
+                         for text in ["Эта планета близка к Земле;",
+                                      "На ней много необходимых ресурсов;",
+                                      "На ней есть вода и атмосфера;",
+                                      "На ней есть небольшое магнитное поле;",
+                                      "Наконец, она просто красива!"]]),
+        "луна": templates[0] + "".join([templates[1].format(choice_color=choice(alerts),
+                                                            choice_font=choice(
+                                                                ["bold", "italic",
+                                                                 "oblique", "line"]),
+                                                            text=text, planet="{planet}")
+                                        for text in ["Эта планета близка к Земле;",
+                                                     "На ней много необходимых ресурсов;",
+                                                     "На ней есть вода и атмосфера;",
+                                                     "На ней есть небольшое магнитное поле;",
+                                                     "Наконец, она просто красива!"]]),
+        "юпитер": templates[0] +
+                  templates[2].format(image=url_for('static', filename="img/jupiter.png"))
+                  + "<div class='bold'>Самая большая планета в Солнечной системе</div>"}
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
