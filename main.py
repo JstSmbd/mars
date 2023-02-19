c12e84a1d2ff6254baef99a1caa4f62b2b820ad0
JstSmbd <arsengoodvin@yandex.ru>
2023-02-20 00:53:44 +0300

    К задаче "Цвет каюты"

--- main.py
+++ main.py
@@ -1,25 +1,18 @@
 from flask import Flask, url_for, request, render_template, redirect
 from flask_wtf import FlaskForm
-from wtforms import IntegerField, PasswordField, BooleanField, SubmitField
+from wtforms import IntegerField, PasswordField, BooleanField, SubmitField, FieldList
 from wtforms.validators import DataRequired
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
-class LoginForm(FlaskForm):
-    id1 = IntegerField('id астронавта', validators=[DataRequired()])
-    pas1 = PasswordField('Пароль астронавта', validators=[DataRequired()])
-    id2 = IntegerField('id капитана', validators=[DataRequired()])
-    pas2 = PasswordField('Пароль капитана', validators=[DataRequired()])
-    submit = SubmitField('Доступ')
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        return redirect('/success')
-    return render_template('training.html', title='Аварийный доступ', form=form)
+@app.route('/table/<string:le>/<int:age>')
+def main(le, age):
+    return render_template('training.html', title='Цвет каюты',
+                           color=f"{'light' if age < 21 else ''}"
+                                 f"{'red' if le == 'female' else 'blue'}.png",
+                           picture="young.png" if age < 21 else "old.png")
 
 
 if __name__ == '__main__':
