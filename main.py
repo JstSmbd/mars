8d96b6adba0ff1b9a20f258810b17b46f5d374c4
JstSmbd <arsengoodvin@yandex.ru>
2023-02-19 20:32:00 +0300

    К задаче "Список профессий"

--- main.py
+++ main.py
@@ -2,9 +2,15 @@ from flask import Flask, url_for, request, render_template
 app = Flask(__name__)
 
 
-@app.route('/training/<string:profession>')
-def index(profession):
-    return render_template('training.html', profession=profession.lower())
+@app.route('/list_prof/<string:mode>')
+def index(mode):
+    return render_template('training.html', mode=mode,
+                           professions=["инженер-исследователь", "пилот", "строитель", "экзобиолог",
+                                        "врач", "инженер по терраформированию", "климатолог",
+                                        "специалист по радиационной защите", "астрогеолог",
+                                        "гляциолог", "инженер жизнеобеспечения", "метеоролог",
+                                        "оператор марсохода", "киберинженер", "штурман",
+                                        "пилот дронов"])
 
 
 if __name__ == '__main__':
