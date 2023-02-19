a60992dacfb7b2615f65d22331d115626db66272
JstSmbd <arsengoodvin@yandex.ru>
2023-02-19 20:52:05 +0300

    К задаче "Автоматический ответ"

--- main.py
+++ main.py
@@ -2,15 +2,20 @@ from flask import Flask, url_for, request, render_template
 app = Flask(__name__)
 
 
-@app.route('/list_prof/<string:mode>')
-def index(mode):
-    return render_template('training.html', mode=mode,
-                           professions=["инженер-исследователь", "пилот", "строитель", "экзобиолог",
-                                        "врач", "инженер по терраформированию", "климатолог",
-                                        "специалист по радиационной защите", "астрогеолог",
-                                        "гляциолог", "инженер жизнеобеспечения", "метеоролог",
-                                        "оператор марсохода", "киберинженер", "штурман",
-                                        "пилот дронов"])
+@app.route('/answer')
+@app.route('/auto_answer')
+def index():
+    params = {
+        "title": "Анкета",
+        "surname": "Watny",
+        "name": "Mark",
+        "education": "выше среднего",
+        "profession": "штурман марсохода",
+        "sex": "male",
+        "motivation": "Всегда мечтал застрять на Марсе!",
+        "ready": True
+    }
+    return render_template('training.html', **params)
 
 
 if __name__ == '__main__':
