81c1b5512e5a30abb749456d9b8397054d49efc5
JstSmbd <arsengoodvin@yandex.ru>
2023-02-20 16:26:10 +0300

    К задаче "Личная карточка"

--- main.py
+++ main.py
@@ -1,27 +1,16 @@
 from flask import Flask, url_for, request, render_template, redirect
-from flask_wtf import FlaskForm
-from wtforms import SubmitField, FileField
-from wtforms.validators import DataRequired
+import json
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
-class AddPhoto(FlaskForm):
-    file = FileField("Добавить картинку", validators=[DataRequired()])
-    submit = SubmitField("Отправить")
-
-
-@app.route('/galery', methods=["GET", "POST"])
+@app.route('/member')
 def main():
-    form = AddPhoto()
-    if request.files:
-        request.files["file"].save(f"static/img/p{int(names[-1][1:]) + 1}.jpg")
-        names.append(f"p{int(names[-1][1:]) + 1}")
-    return render_template('training.html', title="Красная планета", names=names, form=form)
+    with open("templates/people.json", "rt", encoding="utf8") as f:
+        people_list = json.loads(f.read())
+    return render_template('training.html', title="Личная карточка", people=people_list)
 
 
-names = ["p1", "p2", "p3", "p4"]
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
